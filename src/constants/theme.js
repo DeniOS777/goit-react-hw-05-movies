8b199c1f88b96048e1e6eaac1a6f9ace43f76a27
DeniOS777 <Denios777@gmail.com>
2022-09-08 22:34:07 +0300

    add theme and globalStyle

---
+++ src/constants/theme.js
@@ -0,0 +1,52 @@
+export const theme = {
+  colors: {
+    whiteColor: '#ffffff',
+    greenColor: '#089408',
+    greenColorHover: '#078207',
+    neutralColor: '#969696',
+    neutralColorHover: '#868585',
+    redColor: '#d42e2e',
+    redColorHover: '#c12a2a',
+    titleColor: '#009700',
+  },
+
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+
+  fontWeights: {
+    normal: 400,
+    middle: 600,
+    bold: 700,
+  },
+
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from './components/GlobalStyle';
+import { theme } from './constants/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
